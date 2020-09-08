43d0a6b816f4bb73891376195cb69694b67be43e
jgersain <jhersein@gmail.com>
2020-09-08 00:55:52 -0500

    Add contructor method for javas classes

--- POO/Uber/java/Car.java
+++ POO/Uber/java/Car.java
@@ -1,6 +1,17 @@
+import javax.sound.midi.Soundbank;
+
 class Car {
   Integer id;
   String licence;
-  String driver;
+  Account driver;
   Integer passenger;  
+
+  public Car(String licence, Account driver) {
+    this.licence = licence;
+    this.driver = driver;
+  }
+
+  void printDataCar() {
+    System.out.println("Licence: " + licence + " Driver: " +  driver.name);
+  }
 }
--- POO/Uber/java/Main.java
+++ POO/Uber/java/Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+    Car car = new Car("XWLZ5", new Account("John Connor", "OOMG121"));
+    car.printDataCar();
+    Car car2 = new Car("ABCDE", new Account("John Cena", "UNAMMX"));
+    car2.printDataCar();
   }
 }
