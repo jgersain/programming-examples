dc1257f9620330e372b596cb413437c567183828
jgersain <jhersein@gmail.com>
2020-09-08 01:23:05 -0500

    Add contructor method for python classes

--- POO/Uber/python/main.py
+++ POO/Uber/python/main.py
@@ -1,2 +1,9 @@
+from car import Car
+from account import Account
+
 if __name__ == "__main__":
-  print('Hola Mundo')
+  print('Uber')
+
+  car = Car("XWLZ5", Account("John Cena", "CURP"))
+
+  print(vars(car))
