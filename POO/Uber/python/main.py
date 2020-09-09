aff8b93bff5aa67361544f83694b7d76f25060b9
jgersain <jhersein@gmail.com>
2020-09-08 23:16:24 -0500

    Apply inheritance python classes

--- POO/Uber/python/main.py
+++ POO/Uber/python/main.py
@@ -1,9 +1,13 @@
 from car import Car
 from account import Account
+from uberX import UberX
 
 if __name__ == "__main__":
   print('Uber')
 
   car = Car("XWLZ5", Account("John Cena", "CURP"))
+  uber_x = UberX("WSDFGT", Account("John Connor", "CURP0987"), "Chevrolet", "Attitude")
+  uber_black = UberX("ABG67GF", Account("John Travolta", "RFC0987"), ["Chevy"], ["Lana"])
 
-  print(vars(car))
+  print(vars(uber_x))
+  print(vars(uber_black))
